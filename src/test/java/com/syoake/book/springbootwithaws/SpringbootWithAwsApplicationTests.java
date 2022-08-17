302101c42efd8afb29cf50e750e568a796fff097
syoake <ryupark1006@gmail.com>
2022-08-18 02:00:53 +0900

    Initial commit

---
+++ src/test/java/com/syoake/book/springbootwithaws/SpringbootWithAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syoake.book.springbootwithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
