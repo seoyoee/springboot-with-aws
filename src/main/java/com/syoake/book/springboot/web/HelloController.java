1c4ff83c5b9a9d072f11b4796fb6a44a9203184d
syoake <ryupark1006@gmail.com>
2022-08-24 21:31:12 +0900

    [2장] 스프링 부트에서 테스트 코드를 작성하자

--- src/main/java/com/syoake/book/springboot/web/HelloController.java
+++ src/main/java/com/syoake/book/springboot/web/HelloController.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@RestController // json을 반환하는 컨트롤러로 만들어 줌
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello") // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 줌
     public String hello() {
         return "hello";
     }
 
     @GetMapping("/hello/dto")
-    public HelloResponseDto helloDto(@RequestParam("name") String name,
+    public HelloResponseDto helloDto(@RequestParam("name") String name, // 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
                                      @RequestParam("amount") int amount) {
         return new HelloResponseDto(name, amount);
     }
