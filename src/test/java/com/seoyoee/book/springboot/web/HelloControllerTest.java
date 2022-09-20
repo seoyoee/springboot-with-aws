1c7b2f1157b32f501efd2d6d809a096075f2199d
Seoyoung <ryupark1006@gmail.com>
2022-09-21 08:27:56 +0900

    [5장] 기존 테스트에 시큐리티 적용하기

--- src/test/java/com/seoyoee/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/seoyoee/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.seoyoee.book.springboot.web;
 
+import com.seoyoee.book.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test; // Junit5에서 junit -> junit.jupiter.api
 import org.junit.jupiter.api.extension.ExtendWith; // Junit5에서 RunWith -> ExtendWith
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;  // Junit5에서 SpringRunner -> SpringExtension
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class) // 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 함
-@WebMvcTest(controllers = HelloController.class) // Web에 집중할 수 있는 어노테이션
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+) // Web에 집중할 수 있는 어노테이션
 public class HelloControllerTest {
 
     @Autowired // 스프링이 관리하는 빈을 주입받음
     private MockMvc mvc; // 스프링 MVC의 시작점으로 웹 API를 테스트 할 때 사용
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -29,6 +38,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // mvc.perform의 결과를 검증 (응답 본문의 내용)
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
